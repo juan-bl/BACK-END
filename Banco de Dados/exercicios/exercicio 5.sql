6b36a9c1a552c484adcc1c18adc769cb34c108ee
juan-bl <129800235+juan-bl@users.noreply.github.com>
2025-02-17 17:42:26 -0300

    Update exercicio 5.sql

--- Banco de Dados/exercicios/exercicio 5.sql
+++ Banco de Dados/exercicios/exercicio 5.sql
@@ -12,8 +12,20 @@ insert into livros (titulo, ano_publicao, id_autor) values ("Quinze Dias", 2020,
 insert into livros (titulo, ano_publicao, id_autor) values ("Se a Casa 8 Falasse", 2022, 1);
 insert into livros (titulo, ano_publicao, id_autor) values ("Um Milhão de Finais Felizes", 2020, 1);
 insert into livros (titulo, ano_publicao, id_autor) values ("Enquanto Eu Não Te Encontro", 2021, 2);
-insert into livros (titulo, ano_publicao, id_autor) values ("teste", 2025, 3);
 
+
+CREATE TABLE livros_autores (
+	id_autor INT NOT NULL,
+    id_livro INT NOT NULL,
+    FOREIGN KEY (id_autor) REFERENCES autores(id_autor),
+    FOREIGN KEY (id_livro) REFERENCES livros(id_livros)
+);
+
+insert into livros_autores (id_autor, id_livro) values (1, 1);
+insert into livros_autores (id_autor, id_livro) values (2, 2);
+insert into livros_autores (id_autor, id_livro) values (3, 3);
+
+select * from livros_autores;
 select * from autores;
 select * from livros;
 select * from livros where id_autor = 1;
